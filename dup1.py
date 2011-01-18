0dfb617127ac7ffd286ac5e788ce4fb7c8dc9f3c
Anja Berens <pharaun666@gmail.com>
2011-01-17 21:49:36 -0500

    working on killing memory consumption

--- dup1.py
+++ dup1.py
@@ -10,6 +10,9 @@ import sys
 import imghdr
 import hashlib
 import Image
+import numpy as np
+from multiprocessing import Pool
+from guppy import hpy
 
 ################################################################################
 def calc_image_stats(img_list):
@@ -56,31 +59,98 @@ def generate_img_list(rootdir):
                 img.append( (root, filename) )
     return img
 
+
 ################################################################################
 def generate_image_sim(path):
-    return None
+    mode = ('RGB',)
+
+    try:
+        with open(path) as f:
+            image = Image.open(f)
+            if not (image.mode in mode):
+                return None
+
+            width, height = image.size
+
+            x_inc = width / 32
+            y_inc = height / 32
+
+            # For now don't deal with small images
+            if x_inc < 1:
+                return None
+            elif y_inc < 1:
+                return None
+
+            image_pixel = np.asarray(image)
+            img_sum = []
+
+            # Start looping through it
+            j = 0
+            for ys in range(32):
+                i = 0
+                for xs in range(32):
+                    # Sanity check
+                    try:
+                        r = np.mean(image_pixel[i:i+y_inc, j:j+x_inc, 0:1])
+                        g = np.mean(image_pixel[i:i+y_inc, j:j+x_inc, 1:2])
+                        b = np.mean(image_pixel[i:i+y_inc, j:j+x_inc, 2:3])
+                    except IndexError:
+                        return None
+
+                    # Append a rgb tuple to a list
+                    img_sum.append( (r, g, b) )
+
+                    i += x_inc
+                j += y_inc
+
+            return img_sum
+
+    except IOError:
+        return None
 
 
 ################################################################################
 def generate_sim_data(img_list):
     img = []
+#    total = len(img_list)
+#    i = 0
 
+    filepath = []
     for root, filename in img_list:
-        img.append( (root, filename, generate_image_sim(os.path.join(root, filename))) )
+        filepath.append( os.path.join(root, filename))
+
+    # Setup the pools?
+    pool = Pool(processes=24)
+    img = pool.map(generate_image_sim, filepath)
+
+#    for root, filename in img_list:
+#        img.append( (root, filename, generate_image_sim(os.path.join(root, filename))) )
+#
+#        i += 1
+#        print "\r" + str(i) + "/" + str(total) + " - %.2f" % (100.0 * (float(i) / total)) + "%",
+#        sys.stdout.flush()
+
 
     return img
 
 
+if __name__ == '__main__':
+    # Rest of the program
+    if len(sys.argv) != 2:
+        print sys.argv[0] + " <directory to recurse into>"
+        sys.exit(2)
+    rootdir = sys.argv[1]
 
-# Rest of the program
-if len(sys.argv) != 2:
-    print sys.argv[0] + " <directory to recurse into>"
-    sys.exit(2)
-rootdir = sys.argv[1]
+    img = generate_img_list(rootdir)
+    #calc_image_stats(img)
 
-img = generate_img_list(rootdir)
-#calc_image_stats(img)
-sim = generate_sim_data(img)
+    h = hpy()
+    sim = generate_sim_data(img)
+    print h.heap()
+
+    with open('out', 'w') as f:
+        for line in sim:
+            f.write(sim)
 
 
 ## Create a hash and compare the size
